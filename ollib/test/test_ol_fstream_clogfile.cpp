cb9e5a67eafa9fed62e4462f4baab3ba204189e2
OL <1613661434@qq.com>
2025-08-16 22:43:14 +0800

    修改测试用例

--- ollib/test/test_ol_fstream_clogfile.cpp
+++ ollib/test/test_ol_fstream_clogfile.cpp
@@ -11,8 +11,16 @@ int main()
 {
     clogfile logfile; // 创建日志对象。
 
+    string filePath;
+
+#ifdef __linux__
+    filePath = "/tmp/log/test_ol_fstream_clogfile.log";
+#elif defined(_WIN32)
+    filePath = R"(C:\test\test_ol_fstream_clogfile.log)";
+#endif
+
     // 打开日志文件。
-    if (logfile.open("/tmp/log/demo42.log", ios::out, false) == false)
+    if (logfile.open(filePath, ios::out, false) == false)
     {
         printf("logfile.open(/tmp/log/demo42.log) failed.\n");
         return -1;
@@ -20,12 +28,12 @@ int main()
 
     logfile.write("程序开始运行。\n");
 
-    for (int ii = 0; ii < 5000000; ii++)
+    for (size_t i = 0; i <= 1000; ++i)
     {
-        logfile.write("这是第%d个%s...ok.\n", ii, "超级女生");
-        // logfile.write("第%d个超女开始表演...",ii);     // 表演前，写一行日志，...表示正在表演中。
-        // sleep(2);                                                          // 超女在表演中。
-        // logfile << "ok.\n";                                          // 表演完成后，写入ok。
+        logfile.write("这是第%d个%s...ok.\n", i, "超级女生");
+        logfile.write("第%d个超女开始表演...", i); // 表演前，写一行日志，...表示正在表演中。
+        // sleep_ms(2);                                // 超女在表演中。
+        logfile << "ok.\n"; // 表演完成后，写入ok。
     }
 
     logfile.write("程序运行结束。\n");
